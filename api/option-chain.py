2b3409d9c786d3d48024f325297f262432ae279f
uday <udayadds@outlook.com>
2022-02-27 21:01:05 +0530

    Adding logs

--- api/option-chain.py
+++ api/option-chain.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
 		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 		self.send_response(200)
-		self.send_header('Content-type','application/json')
+		self.send_header('Content-type', 'application/json')
 		self.end_headers()
 
 		if 'ticker' in dic:
@@ -19,15 +19,20 @@ class handler(BaseHTTPRequestHandler):
 		baseurl = "https://www.nseindia.com/"
 		# url = f"https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
 		url = f"https://www.nseindia.com/api/option-chain-equities?symbol="+ticker
+		print(url)
 		headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.62',
             'accept-language': 'en,gu;q=0.9,hi;q=0.8', 'accept-encoding': 'gzip, deflate, br'
         }
+        
 		session = requests.Session()
 		request = session.get(baseurl, headers=headers, timeout=5)
 		cookies = dict(request.cookies)
 		response = session.get(url, headers=headers, timeout=5, cookies=cookies)
-		# print(response.json())
+		print(response.json())
+		print(f'Status Code: {response.status_code}')
 
-		self.wfile.write(response.json().encode())
+		result = response.json()
+
+		self.wfile.write(result.encode(encoding='utf_8'))
 		return
