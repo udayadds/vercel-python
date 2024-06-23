3d6b86cd6b59cd4c0bbc2ab5ba2bbb46f605cb2a
udayadds <76723576+udayadds@users.noreply.github.com>
2024-06-23 15:17:21 +0530

    Update option-chain.py

--- api/option-chain.py
+++ api/option-chain.py
@@ -7,7 +7,7 @@ class handler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
+		
 		self.send_header('Content-type', 'application/json')
 		self.end_headers()
 
@@ -18,21 +18,19 @@ class handler(BaseHTTPRequestHandler):
 
 		baseurl = "https://www.nseindia.com/"
 		# url = f"https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
-		url = f"https://www.nseindia.com/api/option-chain-equities?symbol="+ticker
-		print(url)
+		url = "https://nsearchives.nseindia.com/products/content/sec_bhavdata_full_21062024.csv"
+		# print(url)
 		headers = {
-		    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.62',
-		    'accept-language': 'en,gu;q=0.9,hi;q=0.8', 'accept-encoding': 'gzip, deflate, br'
 		}
         
 		session = requests.Session()
-		request = session.get(baseurl, headers=headers, timeout=5)
+		request = session.get(baseurl, headers=headers, timeout=10)
 		cookies = dict(request.cookies)
-		response = session.get(url, headers=headers, timeout=5, cookies=cookies)
+		response = session.get(url, headers=headers, timeout=10, cookies=cookies)
 		# print(response.json())
-		print(f'Status Code: {response.status_code}')
-
+		# print(f'Status Code: {response.status_code}')
 		# result = response.json()
 
+		self.send_response(response.status_code)
 		self.wfile.write(response.content)
 		return
